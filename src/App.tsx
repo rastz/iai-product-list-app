9898989b255190972d1143e90be8421203ae457f
Mateusz Podgórski <32940609+rastz@users.noreply.github.com>
2025-05-03 12:43:09 +0200

    feat: use components in main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,62 @@
+import { Box } from "./components/Box";
+import { Button } from "./components/Button";
+import { Grid } from "./components/Grid";
+import { Main } from "./components/Main";
+import { Section } from "./components/Section";
+import { Table } from "./components/Table";
+import { TableBody } from "./components/TableBody";
+import { TableCaption } from "./components/TableCaption";
+import { TableCell } from "./components/TableCell";
+import { TableHead } from "./components/TableHead";
+import { TableHeader } from "./components/TableHeader";
+import { TableRow } from "./components/TableRow";
+
 function App() {
   return (
-    <>
-      <table>
-        <caption>Product List</caption>
+    <Main>
+      <Section>
+        <Box>
+          <Table>
+            <TableCaption>Product List</TableCaption>
 
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Icon</th>
-            <th scope="col">Price</th>
-            <th scope="col">Stock</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
+            <TableHeader>
+              <TableRow>
+                <TableHead scope="col">ID</TableHead>
+                <TableHead scope="col">Name</TableHead>
+                <TableHead scope="col">Icon</TableHead>
+                <TableHead scope="col">Price</TableHead>
+                <TableHead scope="col">Stock</TableHead>
+                <TableHead scope="col">Actions</TableHead>
+              </TableRow>
+            </TableHeader>
 
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
+            <TableBody>
+              <TableRow>
+                <TableHead scope="row">1</TableHead>
 
-            <td>Product Name</td>
-            <td>
-              <img
-                src="https://placehold.co/600x400?font=montserrat"
-                alt=""
-                width="50"
-                height="50"
-              />
-            </td>
-            <td>$99.99</td>
-            <td>37</td>
-            <td>
-              <button>Edit</button>
-              <button>Delete</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </>
+                <TableCell>Product Name</TableCell>
+                <TableCell>
+                  <img
+                    src="https://placehold.co/600x400?font=montserrat"
+                    alt=""
+                    width="50"
+                    height="50"
+                  />
+                </TableCell>
+                <TableCell>$99.99</TableCell>
+                <TableCell>37</TableCell>
+                <TableCell>
+                  <Grid>
+                    <Button variant="primary">Edit</Button>
+                    <Button variant="secondary">Delete</Button>
+                  </Grid>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Box>
+      </Section>
+    </Main>
   );
 }
 
