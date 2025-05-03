4ec40927a9abf776fbed45a5a7bafe80c278580b
Mateusz Podgórski <32940609+rastz@users.noreply.github.com>
2025-05-03 13:29:30 +0200

    fix: make list wider

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,7 +3,7 @@ import { ComponentProps } from "react";
 type SectionProps = ComponentProps<"section">;
 
 function Section({ children }: SectionProps) {
-  return <section className="mx-auto max-w-5xl px-5 py-10">{children}</section>;
+  return <section className="mx-auto max-w-7xl px-5 py-10">{children}</section>;
 }
 
 export { Section };
