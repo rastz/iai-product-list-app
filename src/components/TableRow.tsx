acbfdc4046c8916cab1d44e965235fe0cadbe5fb
Mateusz Podgórski <32940609+rastz@users.noreply.github.com>
2025-05-03 13:30:06 +0200

    feat: change rows divider color

--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -5,7 +5,7 @@ type TableRowProps = ComponentProps<"tr">;
 function TableRow({ children, ...restProps }: TableRowProps) {
   return (
     <tr
-      className={"hover:bg-gray-50 border-b border-b-gray-500"}
+      className={"hover:bg-gray-50 border-b border-b-gray-300"}
       {...restProps}
     >
       {children}
