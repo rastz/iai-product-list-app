e3d9f01b1d16dbcd011974737844d8b0a7c696da
Mateusz Podgórski <32940609+rastz@users.noreply.github.com>
2025-05-04 21:23:26 +0200

    feat(DialogModal): render as drawer on mobile

--- src/components/ConfirmDialog.tsx
+++ src/components/ConfirmDialog.tsx
@@ -1,6 +1,8 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { Button } from "./Button";
 import { ReactNode } from "react";
+import clsx from "clsx";
+import { useMedia } from "react-use";
 
 interface ConfirmDialogProps {
   open: boolean;
@@ -13,6 +15,9 @@ interface ConfirmDialogProps {
   onCancel: VoidFunction;
 }
 
+const BASE_DIALOG_CLASSES =
+  "fixed right-0 bottom-0 left-0 z-50 flex max-h-1/2 flex-col gap-y-4 overflow-auto rounded-t-xl bg-white p-4 shadow-lg md:top-1/2 md:right-auto md:bottom-auto md:left-1/2 md:max-w-sm md:-translate-x-1/2 md:-translate-y-1/2 md:animate-none md:rounded-lg md:p-6";
+
 export function ConfirmDialog({
   open,
   onOpenChange,
@@ -23,11 +28,21 @@ export function ConfirmDialog({
   onConfirm,
   onCancel,
 }: ConfirmDialogProps) {
+  /**
+   * This 'max-width' match with Tailwind 'md' breakpoint
+   */
+  const md = useMedia(`(max-width: 768px`);
+
   return (
     <Dialog.Root open={open} onOpenChange={onOpenChange}>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-black/40 z-40" />
-        <Dialog.Content className="fixed top-1/2 left-1/2 z-50 max-w-sm -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-lg shadow-lg flex flex-col gap-y-4">
+        <Dialog.Overlay className="fixed inset-0 z-40 bg-black/40" />
+        <Dialog.Content
+          className={clsx(BASE_DIALOG_CLASSES, {
+            "animate-slideUp": md,
+            "md:animate-none": !md,
+          })}
+        >
           <Dialog.Title className="text-lg font-medium">{title}</Dialog.Title>
           {description && (
             <Dialog.Description className="text-sm text-gray-600">
@@ -35,7 +50,7 @@ export function ConfirmDialog({
             </Dialog.Description>
           )}
 
-          <div className="flex justify-end gap-2 mt-4">
+          <div className="mt-4 flex justify-end gap-2">
             <Dialog.Close asChild>
               <Button variant="secondary" onClick={onCancel}>
                 {cancelLabel}
