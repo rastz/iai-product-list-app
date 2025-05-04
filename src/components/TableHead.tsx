efc87756c085a8e653d8be13b409be2f12f7febf
Mateusz Podgórski <32940609+rastz@users.noreply.github.com>
2025-05-04 13:28:21 +0200

    fix: prevent text selection on table heade

--- src/components/TableHead.tsx
+++ src/components/TableHead.tsx
@@ -5,7 +5,7 @@ type TableHeadProps = ComponentProps<"th">;
 function TableHead({ children, ...restProps }: TableHeadProps) {
   return (
     <th
-      className="p-10 text-center align-middle font-medium whitespace-nowrap"
+      className="p-10 text-center align-middle font-medium whitespace-nowrap select-none"
       {...restProps}
     >
       {children}
