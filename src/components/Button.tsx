c5faf0aff148f32ec56460b0342d96bdfd6d0bd6
Mateusz Podgórski <32940609+rastz@users.noreply.github.com>
2025-05-04 13:27:47 +0200

    fix: prevent text selection on button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,9 +8,9 @@ interface ButtonProps extends ComponentProps<"button"> {
 
 const VARIANTS: Record<Variant, string> = {
   primary:
-    "bg-gray-900 rounded-sm px-4 py-2 text-white cursor-pointer hover:bg-gray-800",
+    "bg-gray-900 rounded-sm px-4 py-2 text-white cursor-pointer hover:bg-gray-800 select-none",
   secondary:
-    "bg-gray-50 rounded-sm px-4 py-2 text-gray-900 cursor-pointer hover:bg-gray-100 border border-gray-200",
+    "bg-gray-50 rounded-sm px-4 py-2 text-gray-900 cursor-pointer hover:bg-gray-100 border border-gray-200 select-none",
 };
 
 function Button({ children, variant, ...restProps }: ButtonProps) {
