0502145d418b3b4d420f287425901b73292eca08
Mateusz Podgórski <32940609+rastz@users.noreply.github.com>
2025-05-04 16:26:08 +0200

    feat: add 'FiltersButton' component

---
+++ src/components/FiltersButton.tsx
@@ -0,0 +1,38 @@
+import { Table } from "@tanstack/react-table";
+import { Product } from "../types";
+import { Button } from "./Button";
+import { useState } from "react";
+import { RangeFilter } from "./RangeFilter";
+
+interface FiltersButtonProps {
+  table: Table<Product>;
+}
+
+function FiltersButton({ table }: FiltersButtonProps) {
+  const [filtersOpen, setFiltersOpen] = useState(false);
+
+  const priceColumn = table.getColumn("price");
+  const stockColumn = table.getColumn("stock");
+
+  return (
+    <div className="relative inline-block">
+      <Button
+        variant="primary"
+        icon="filters"
+        onClick={() => setFiltersOpen((prev) => !prev)}
+      >
+        Filters
+      </Button>
+
+      {filtersOpen && (
+        <div className="absolute right-0 mt-2 p-4 w-72 z-50 bg-white border rounded shadow-lg">
+          {priceColumn && <RangeFilter column={priceColumn} label="Price" />}
+          <hr className="my-2" />
+          {stockColumn && <RangeFilter column={stockColumn} label="Stock" />}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export { FiltersButton };
