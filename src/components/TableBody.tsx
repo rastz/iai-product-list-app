1bb8fcc494322fb832834359dc0f3d6d4c42f104
Mateusz Podgórski <32940609+rastz@users.noreply.github.com>
2025-05-04 12:21:26 +0200

    feat: remove alternating row background from table

--- src/components/TableBody.tsx
+++ src/components/TableBody.tsx
@@ -5,7 +5,7 @@ type TableBodyProps = ComponentProps<"tbody">;
 function TableBody({ children, ...restProps }: TableBodyProps) {
   return (
     <tbody
-      className="[&_tr:last-child]:border-0 [&_tr]:hover:bg-gray-100 [&_tr]:even:bg-gray-50"
+      className="[&_tr:last-child]:border-0 [&_tr]:hover:bg-gray-50"
       {...restProps}
     >
       {children}
