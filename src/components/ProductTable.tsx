321ad30fe2729dbfe7037b3ebfe9bc37b2d6673c
Mateusz Podgórski <32940609+rastz@users.noreply.github.com>
2025-05-04 15:23:25 +0200

    feat: add sort and filter to list

--- src/components/ProductTable.tsx
+++ src/components/ProductTable.tsx
@@ -2,7 +2,9 @@ import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   getSortedRowModel,
+  SortingState,
   useReactTable,
 } from "@tanstack/react-table";
 import { Product } from "../types";
@@ -15,6 +17,9 @@ import { TableHead } from "./TableHead";
 import { TableBody } from "./TableBody";
 import { Grid } from "./Grid";
 import { Button } from "./Button";
+import { Fragment, useState } from "react";
+import { SortableTableHead } from "./SortableTableHead";
+import { Input } from "./Input";
 
 interface ProductTableProps {
   data: Product[];
@@ -24,7 +29,10 @@ const columnHelper = createColumnHelper<Product>();
 
 const columns = [
   columnHelper.accessor("img", {
-    header: "Icon",
+    id: "img",
+    enableSorting: false,
+    enableGlobalFilter: false,
+    header: (info) => <TableHead key={info.header.id}>Icon</TableHead>,
     cell: (info) => {
       const { url, alt } = info.getValue();
       return (
@@ -33,30 +41,71 @@ const columns = [
           alt={alt}
           width="50"
           height="50"
-          className="rounded-sm"
+          className="rounded-sm select-none"
         />
       );
     },
   }),
   columnHelper.accessor("name", {
-    header: "Name",
+    id: "name",
+    enableSorting: true,
+    enableGlobalFilter: true,
+    header: ({ column }) => (
+      <SortableTableHead
+        onClick={column.getToggleSortingHandler()}
+        sortDirection={column.getIsSorted()}
+      >
+        Name
+      </SortableTableHead>
+    ),
     cell: (info) => info.getValue(),
   }),
   columnHelper.accessor("price", {
-    header: "Price",
+    id: "price",
+    enableSorting: true,
+    enableGlobalFilter: true,
+    header: ({ column }) => (
+      <SortableTableHead
+        onClick={column.getToggleSortingHandler()}
+        sortDirection={column.getIsSorted()}
+      >
+        Price
+      </SortableTableHead>
+    ),
     cell: (info) => `$${info.getValue()}`,
   }),
   columnHelper.accessor("stock", {
-    header: "Stock",
+    id: "stock",
+    enableSorting: true,
+    enableGlobalFilter: true,
+    header: ({ column }) => (
+      <SortableTableHead
+        onClick={column.getToggleSortingHandler()}
+        sortDirection={column.getIsSorted()}
+      >
+        Stock
+      </SortableTableHead>
+    ),
     cell: (info) => info.getValue(),
   }),
   columnHelper.accessor("id", {
-    header: "ID",
+    id: "id",
+    enableSorting: true,
+    enableGlobalFilter: true,
+    header: ({ column }) => (
+      <SortableTableHead
+        onClick={column.getToggleSortingHandler()}
+        sortDirection={column.getIsSorted()}
+      >
+        ID
+      </SortableTableHead>
+    ),
     cell: (info) => info.getValue(),
   }),
   columnHelper.display({
     id: "action",
-    header: "Action",
+    enableSorting: false,
+    header: () => <TableHead>Action</TableHead>,
     cell: () => (
       <Grid>
         <Button variant="primary">Edit</Button>
@@ -67,44 +116,70 @@ const columns = [
 ];
 
 function ProductTable({ data }: ProductTableProps) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [globalFilter, setGlobalFilter] = useState("");
+
   const table = useReactTable({
     data,
     columns,
+    state: { sorting, globalFilter },
+    globalFilterFn: "includesString",
+    onSortingChange: setSorting,
+    onGlobalFilterChange: setGlobalFilter,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
   });
 
   return (
-    <Table>
-      <TableCaption>Product List</TableCaption>
+    <div className="flex flex-col gap-y-4">
+      <h1 className="text-5xl font-bold text-neutral-700 text-left">
+        Product List
+      </h1>
+      <div className="flex gap-x-1 md:justify-end">
+        <Input
+          type="text"
+          placeholder="Search..."
+          value={globalFilter}
+          onChange={(event) => setGlobalFilter(event.target.value)}
+        />
 
-      <TableHeader>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <TableRow key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <TableHead key={header.id}>
-                {flexRender(
-                  header.column.columnDef.header,
-                  header.getContext()
-                )}
-              </TableHead>
-            ))}
-          </TableRow>
-        ))}
-      </TableHeader>
+        <Button variant={"primary"} icon="filters">
+          Filters
+        </Button>
+      </div>
 
-      <TableBody>
-        {table.getRowModel().rows.map((row) => (
-          <TableRow key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <TableCell key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </TableCell>
-            ))}
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <Fragment key={header.id}>
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </Fragment>
+              ))}
+            </TableRow>
+          ))}
+        </TableHeader>
+
+        <TableBody>
+          {table.getRowModel().rows.map((row) => (
+            <TableRow key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+
+        <TableCaption>End of list</TableCaption>
+      </Table>
+    </div>
   );
 }
 
