a1448f6909b2e41f71db5cddeaa0d4a7f4d5bd2f
Mateusz Podgórski <32940609+rastz@users.noreply.github.com>
2025-05-05 21:03:25 +0200

    feat(Button): add 'loading' variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,8 @@
 import { ComponentProps, ReactNode } from "react";
 import { FilterIcon } from "./Icons/FilterIcon";
+import { SpinnerIcon } from "./Icons/SpinnerIcon";
 
-type Variant = "primary" | "secondary" | "danger";
+type Variant = "primary" | "secondary" | "danger" | "loading";
 type Icon = "filters";
 
 interface ButtonProps extends ComponentProps<"button"> {
@@ -18,19 +19,35 @@ const VARIANTS: Record<Variant, string> = {
   secondary:
     "bg-gray-50 text-gray-900 hover:bg-gray-100 border border-gray-200",
   danger: "bg-red-700 text-white hover:bg-red-800",
+  loading: "text-black bg-gray-200",
 };
 
 const DEFAULT_CLASSES =
   "rounded-sm px-4 py-2 cursor-pointer select-none grid auto-cols-max gap-2 place-content-center grid-flow-col";
 
 function Button({ children, variant, icon, ...restProps }: ButtonProps) {
+  const isLoading = variant === "loading";
+
+  const loadingJSX = (
+    <span className="h-6 w-6 animate-spin">
+      <SpinnerIcon />
+    </span>
+  );
+
+  const contentJSX = (
+    <>
+      {icon && <span className="h-6 w-6">{ICONS[icon]}</span>}
+      {children}
+    </>
+  );
+
   return (
     <button
+      disabled={isLoading}
       className={`${DEFAULT_CLASSES} ${VARIANTS[variant]}`}
       {...restProps}
     >
-      {icon && <span className="w-6 h-6">{ICONS[icon]}</span>}
-      {children}
+      {isLoading ? loadingJSX : contentJSX}
     </button>
   );
 }
