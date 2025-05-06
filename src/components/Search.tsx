73119537a5f1e427f3bd73186a6ef42bfae489bd
Mateusz Podgórski <32940609+rastz@users.noreply.github.com>
2025-05-06 19:54:01 +0200

    fix(Search): grow to parent width

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,7 +3,7 @@ import { ComponentProps } from "react";
 type SearchProps = ComponentProps<"input">;
 
 function Search(props: SearchProps) {
-  return <input className="block rounded border px-2 py-1" {...props} />;
+  return <input className="block w-full rounded border px-2 py-1" {...props} />;
 }
 
 export { Search };
