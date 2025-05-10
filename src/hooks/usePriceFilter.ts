e1cef36e7515c3f8313359fbaf63cf03e35a1427
Mateusz Podgórski <32940609+rastz@users.noreply.github.com>
2025-05-10 13:35:09 +0200

    feat: add 'usePriceFilter' hook

---
+++ src/hooks/usePriceFilter.ts
@@ -0,0 +1,26 @@
+import { useState, useMemo, Dispatch, SetStateAction } from "react";
+import { Product } from "../types";
+
+export interface UsePriceFilterHook {
+  filteredData: Product[];
+  minPrice: number | undefined;
+  maxPrice: number | undefined;
+  setMinPrice: Dispatch<SetStateAction<number | undefined>>;
+  setMaxPrice: Dispatch<SetStateAction<number | undefined>>;
+}
+
+export function usePriceFilter(data: Product[]): UsePriceFilterHook {
+  const [minPrice, setMinPrice] = useState<number | undefined>(undefined);
+  const [maxPrice, setMaxPrice] = useState<number | undefined>(undefined);
+
+  const filteredData = useMemo(() => {
+    return data.filter(({ price }) => {
+      if (minPrice !== undefined && price < minPrice) return false;
+      if (maxPrice !== undefined && price > maxPrice) return false;
+
+      return true;
+    });
+  }, [data, minPrice, maxPrice]);
+
+  return { filteredData, minPrice, maxPrice, setMinPrice, setMaxPrice };
+}
